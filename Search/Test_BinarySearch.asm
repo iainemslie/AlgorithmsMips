826400e5e2ec14b1a653aa76a1c4a6addecb4ce9
Iain Emslie <iainemslie@gmail.com>
2023-10-07 12:30:17 -0400

    Update output

--- Search/Test_BinarySearch.asm
+++ Search/Test_BinarySearch.asm
@@ -1,6 +1,6 @@
 .data
-	#array: .word 9, 7, 8, 42, 6, 4, 5, 3, 1, 2
-	array: .word 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+	array: .word 9, 7, 8, 42, 6, 4, 5, 3, 1, 2
+	#array: .word 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 	searchValue: .word 11
 	length: .word 10
 	welcomeMessage: .asciiz "Binary Search: \n"
@@ -11,6 +11,7 @@
 	valueStr: .asciiz " value: "
 	newLine: .asciiz "\n"
 	indexResultStr: .asciiz "\nIndex of Search Value: "
+	searchValueStr: .asciiz "\nSearch Value: "
 
 .text
 	main:
@@ -19,14 +20,14 @@
 		syscall
 	
 		# Print the array
-		#la $a1, length
-		#jal printArray
+		la $a1, length
+		jal printArray
 		
 		# Sort the array
-		#add $s0, $zero, $ra # Save return address
-		#la $a1, length
-		#jal bubbleSort
-		#add $ra, $zero, $s0 # Restore return address
+		add $s0, $zero, $ra # Save return address
+		la $a1, length
+		jal bubbleSort
+		add $ra, $zero, $s0 # Restore return address
 	
 		# Call Binary Search (expects sorted array)
 		la $a0, array   			# address of the array
@@ -34,19 +35,23 @@
 		lw $a2, searchValue 	# value we're looking for
 		li $a3, 4							# size of array elements (bytes)
 		jal binarySearch
+		add $s0, $zero, $v0 # store index at $a0 (divide by bytes)
+		
+		# Print search value
+		li $v0, 4
+		la $a0, searchValueStr
+		syscall
+		li $v0, 1
+		add $a0, $zero, $a2
+		syscall
 		
 		# Print index of value
-		add $s0, $zero, $v0 # store index at $a0 (divide by bytes)
 		li $v0, 4
 		la $a0, indexResultStr
 		syscall
 		add $a0, $zero, $s0
 		li $v0, 1
 		syscall
-		
-		# Print the array
-		#la $a1, length
-		#jal printArray
 	
 	exit:
 		li $v0, 10
